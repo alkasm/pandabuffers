893bb34bc73cc77bad439f7dea1f837eaf5b8355
Kimberly Jones <mathjones42@gmail.com>
2022-07-10 01:34:13 -0700

    Add main functions

---
+++ pandabuffers/proto_to_pandas.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+from ast import Raise
+
+from typing import Sequence, Optional, Type, Any
+
+from google.protobuf.message import Message
+from google.protobuf.json_format import MessageToJson
+
+import pandas as pd
+
+
+def proto_normalize(
+    protos: Sequence[dict[str, Any]] | Sequence[Message],
+    type: Optional[Type[Message]] = None,
+    including_repeated_fields: bool = False,
+    including_default_value_fields: bool = True,
+    preserving_proto_field_name: bool = True,
+    use_integers_for_enums: bool = True,
+) -> pd.DataFrame:
+    pass
+
+
+def proto_explode(
+    protos: Sequence[dict[str, Any]] | Sequence[Message],
+    path: str,
+    type: Optional[Type[Message]] = None,
+    including_repeated_fields: bool = False,
+    including_default_value_fields: bool = True,
+    preserving_proto_field_name: bool = True,
+    use_integers_for_enums: bool = True,
+) -> pd.DataFrame:
+    pass
